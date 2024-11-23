80b87ee627a86e87d8b9fbefecc856d44fb8f8d5
Roneru-k <152578088+Roneru-k@users.noreply.github.com>
2024-11-22 22:37:48 -0400

    Create Form1.cs

---
+++ Form1.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CALCULADORA
+
+{
+
+    public partial class Form1 : Form
+    {
+
+        double valor = 0;
+        string operacion = "";
+        bool operadorPresionado = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        
+
+        
